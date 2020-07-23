25af787475f08e6ed16dc4a65bb4f897d0dc8471
mattt333 <mat.malric@gmail.com>
2020-07-23 10:38:48 +0200

    feat/homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CompaniesComponent } from './pages/companies/companies.component';
+import { NewcoComponent } from './pages/newco/newco.component';
+import { FollowComponent } from './pages/follow/follow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavBarComponent,
+    CompaniesComponent,
+    NewcoComponent,
+    FollowComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/newco/newco.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newco',
+  templateUrl: './newco.component.html',
+  styleUrls: ['./newco.component.scss']
+})
+export class NewcoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
